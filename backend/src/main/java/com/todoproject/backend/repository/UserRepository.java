ed2c168900a898528095ddea79177d607db46373
dongnyuk <dongnyuk.jun@gmail.com>
2025-06-18 17:34:39 +0900

    Connect Mysql from Mac

---
+++ backend/src/main/java/com/todoproject/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.todoproject.backend.repository;
+
+import com.todoproject.backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+// 사용자 정보 repository
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    Optional<User> findByUserId(String userId);     // 로그인 시 사용
+    boolean existsByUserId(String userId);          // 중복 검사
+}
